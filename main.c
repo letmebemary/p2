74f2e444efbc3c21e9615f6431109eb067f0efe0
Jerónimo Pardo <jeropardor3@gmail.com>
2020-03-04 00:38:48 +0100

    comentarios 2

--- main.c
+++ main.c
@@ -20,14 +20,16 @@
 #include "dynamic_list.h"
 #endif
 #ifdef TEST_LIST
+
 #include "list/list.h"
+
 #endif
 
 tList list;                         //declaración lista de tipo tList
 int vote_null, total_votes;         //votos nulos y votos totales
 
 
-void new(char param[NAME_LENGTH_LIMIT+1]) {             //función que crea un nuevo partido
+void new(char param[NAME_LENGTH_LIMIT + 1]) {             //función que crea un nuevo partido
     tItemL item1;
     strcpy(item1.partyName, param);                     //introduccion de param en item1.partyname
     item1.numVotes = 0;
@@ -35,42 +37,46 @@ void new(char param[NAME_LENGTH_LIMIT+1]) {             //función que crea un n
     printf("* New: party %s\n", item1.partyName);
 }
 
-void stats (int param_int) {                          //función que muestra los votos de cada partido y la participación
+void stats(int param_int) {                          //función que muestra los votos de cada partido y la participación
     tItemL item1;
     tPosL pos;
     if (!isEmptyList(list)) {                         //comprobacion de lista vacia
         item1 = getItem(first(list), list);             //item1 toma el valor del primer elemento de la lista
         pos = first(list);
-        printf("Party %s numvotes %d (%.2f %%)\n", item1.partyName, item1.numVotes,((float)item1.numVotes/param_int)*100);
+        printf("Party %s numvotes %d (%.2f %%)\n", item1.partyName, item1.numVotes,
+               ((float) item1.numVotes / param_int) * 100);
 
-        while(next(pos,list) != LNULL){                 //muestra las stats de cada partido si hay 2do
-            item1 = getItem(next(pos,list),list);
-            pos = next(pos,list);
-            printf("Party %s numvotes %d (%.2f %%)\n", item1.partyName, item1.numVotes,((float)item1.numVotes/param_int)*100);
+        while (next(pos, list) != LNULL) {                 //muestra las stats de cada partido si hay 2do
+            item1 = getItem(next(pos, list), list);
+            pos = next(pos, list);
+            printf("Party %s numvotes %d (%.2f %%)\n", item1.partyName, item1.numVotes,
+                   ((float) item1.numVotes / param_int) * 100);
         }
-    }else{
+    } else {
         printf("+ Error: none parties found");         //mensaje de error en caso de lista vacia
     }
     printf("Null votes %d\n", vote_null);
-    printf("Participation: %d votes from 10 voters (%.2f %%)\n", total_votes+vote_null,((float)(total_votes+vote_null)/param_int)*100);
+    printf("Participation: %d votes from 10 voters (%.2f %%)\n", total_votes + vote_null,
+           ((float) (total_votes + vote_null) / param_int) * 100);
 }
 
-void vote (char param[NAME_LENGTH_LIMIT+1]) {           //función que toma nota de los votos
+void vote(char param[NAME_LENGTH_LIMIT + 1]) {           //función que toma nota de los votos
     tItemL item1;
-    if (findItem(param,list) == LNULL) {                //en el caso de que ese partido no exista voto nulo
+    if (findItem(param, list) == LNULL) {                //en el caso de que ese partido no exista voto nulo
         printf("+ Error: Vote not possible. Party %s not found. NULLVOTE\n", param);
         vote_null += 1;
-    }else {
+    } else {
         item1 = getItem(findItem(param, list), list);
-        updateVotes(item1.numVotes + 1, findItem(param, list),&list);                   //suma 1 voto a un partido
+        updateVotes(item1.numVotes + 1, findItem(param, list), &list);                   //suma 1 voto a un partido
         printf("* Vote: party %s numvotes %d\n", item1.partyName, item1.numVotes + 1);
         total_votes += 1;                              //aumento de votos totales
     }
 }
 
-void processCommand(char command_number[CODE_LENGTH+1], char command, char param[NAME_LENGTH_LIMIT+1]) {      //funcion que determina la acción a llevar a cabo
+void processCommand(char command_number[CODE_LENGTH + 1], char command,
+                    char param[NAME_LENGTH_LIMIT + 1]) {      //funcion que determina la acción a llevar a cabo
     printf("********************\n");                 //separador de intrucciones
-    switch(command) {
+    switch (command) {
         case 'N': {     //crear un partido
             printf("%s %c: party %s\n", command_number, command, param);
             new(param);
@@ -95,14 +101,14 @@ void processCommand(char command_number[CODE_LENGTH+1], char command, char param
 
 void readTasks(char *filename) {                      //funcion que lee el archivo
     FILE *df;
-    char command_number[CODE_LENGTH+1], command, param[NAME_LENGTH_LIMIT+1];
+    char command_number[CODE_LENGTH + 1], command, param[NAME_LENGTH_LIMIT + 1];
 
     df = fopen(filename, "r");                 //abre filename y lo lee
     if (df != NULL) {
         while (!feof(df)) {
             char format[16];
             sprintf(format, "%%%is %%c %%%is", CODE_LENGTH, NAME_LENGTH_LIMIT);
-            fscanf(df,format, command_number, &command, param);
+            fscanf(df, format, command_number, &command, param);
             processCommand(command_number, command, param);     //llamada a la funcion encargada de repartir tareas
         }
         fclose(df);                                  //cierre del fichero

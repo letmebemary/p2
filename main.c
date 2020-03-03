6b34ce2caabfd464aea047d22e8322d9589884db
Jerónimo Pardo <jeropardor3@gmail.com>
2020-03-04 00:41:53 +0100

    comentarios

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 /*
  * TITLE: PROGRAMMING II LABS
  * SUBTITLE: Practical 1
- * AUTHOR 1: ***************************** LOGIN 1: **********
- * AUTHOR 2: ***************************** LOGIN 2: **********
- * GROUP: *.*
+ * AUTHOR 1: Pardo Rodriguez, Jeronimo  LOGIN 1: j.pardo
+ * AUTHOR 2: Martinez Sotelo, Maria LOGIN 2: maria.martinezs
+ * GROUP: 4.3
  * DATE: ** / ** / **
  */
 

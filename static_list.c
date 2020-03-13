da3e9a35168b920db7f966458106916d3c39826e
María <61469721+letmebemary@users.noreply.github.com>
2020-03-13 20:45:17 +0100

    Update static_list.c

--- static_list.c
+++ static_list.c
@@ -9,27 +9,27 @@
 
 #include "static_list.h"
 
-void createEmptyList (tList* L) {
+void createEmptyList (tList* L) {                       //función que crea una lista vacía
     L->lastP = LNULL;
 }
 
-bool insertItem(tItemL d, tPosL p, tList* L) {
-    if(L->lastP == SIZE - 1)
+bool insertItem(tItemL d, tPosL p, tList* L) {          //función que inserta un elemento en la lista
+    if(L->lastP == SIZE - 1)                            //caso lista llena
         return false;
     else {
         L->lastP++;
-        if (p == LNULL) {
+        if (p == LNULL) {                                //caso posición nula
             L->data[L->lastP] = d;
-        } else {
-                for (int i = L->lastP; i > p; i--)
-                    L->data[i] = L->data[i-1];
-                L->data[p] = d;
+        } else {                                         //caso de inserción en el medio
+            for (int i = L->lastP; i > p; i--)
+                L->data[i] = L->data[i-1];
+            L->data[p] = d;
         }
         return true;
     }
 }
 
-bool copyList(tList L, tList* M) {
+bool copyList(tList L, tList* M) {                      //función que copia una lista
     tPosL p;
     for (p = 0; p <= L.lastP; p++)
         M->data[p] = L.data[p];
@@ -37,11 +37,11 @@ bool copyList(tList L, tList* M) {
     return true;
 }
 
-void updateVotes(tNumVotes d, tPosL p, tList* L) {
+void updateVotes(tNumVotes d, tPosL p, tList* L) {      //función que actualiza los votos de cada partido
     L->data[p].numVotes = d;
 }
 
-void deleteAtPosition(tPosL p, tList* L) {
+void deleteAtPosition(tPosL p, tList* L) {              //función que elimina una posición
     tPosL q;
     L->lastP--;
     for (q = p; q <= L->lastP; q++)
@@ -53,9 +53,9 @@ void deleteList(tList* L) { //acabar
 
 }
 
-tPosL findItem(tPartyName d, tList L) {
+tPosL findItem(tPartyName d, tList L) {                 //función que devuelve la posición de un partido en la lista
     tPosL p;
-    if (L.lastP == LNULL)
+    if (L.lastP == LNULL)                               //caso lista vacía
         return LNULL;
     else {
         for (p = 0; (p < L.lastP) && (strcmp(L.data[p].partyName, d) != 0); p++);
@@ -66,28 +66,28 @@ tPosL findItem(tPartyName d, tList L) {
     }
 }
 
-bool isEmptyList(tList L) {
+bool isEmptyList(tList L) {                         //función que comprueba que una lista está vacía
     return L.lastP == LNULL;
 }
 
-tItemL getItem(tPosL p, tList L) {
+tItemL getItem(tPosL p, tList L) {                  //función que devuelve el contenido de una posición de la lista
     return L.data[p];
 }
 
-tPosL first(tList L) {
+tPosL first(tList L) {                              //función que devuelve la primera posición de la lista
     return 0;
 }
 
-tPosL last(tList L) { //mirar que es laspos == iguana
+tPosL last(tList L) {                               //función que devuelve la última posición no vacía de la lista
     return L.lastP;
 }
 
-tPosL previous(tPosL p, tList L) {
+tPosL previous(tPosL p, tList L) {                  //función que devuelve la posición anterior a una dada
     return --p;
 }
 
-tPosL next(tPosL p, tList L) {
-    if (p == last(L))
+tPosL next(tPosL p, tList L) {                      //función que devuelve la posición siguiente a una dada
+    if (p == last(L))                               //caso del último de la lista
         return LNULL;
     else
         return ++p;
